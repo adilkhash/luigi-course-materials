d7e9fb6e18d360c0aa975fb17a9b63aefc2c970c
adilkhash <adil@playrix.com>
2019-10-05 17:49:07 +0600

    init

---
+++ provision.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+bootstrap() {
+  sudo apt-get update
+  sudo apt-get upgrade
+
+  echo "Installing system libs..."
+  echo ""
+  sudo apt-get -y install build-essential
+  sudo apt-get -y install python3-dev
+  sudo apt-get -y install python3-venv
+  sudo apt-get -y install libxml2-dev libxslt1-dev
+  sudo apt-get -y install lib32z1-dev
+  sudo apt-get -y install python-virtualenv
+  sudo apt-get -y install libtiff5-dev
+  sudo apt-get -y install libffi-dev
+  sudo apt-get -y install libfreetype6-dev
+  sudo apt-get -y install libpq-dev
+  sudo apt-get -y install python-setuptools python-pip
+  sudo apt-get -y install libssl-dev
+  sudo apt-get -y install swig
+  sudo apt-get -y install git
+  sudo apt-get -y install gettext
+  sudo pip install virtualenvwrapper
+}
+
+export DEBIAN_FRONTEND=noninteractive
+
+bootstrap
