ca7ad221a9d9e5e69f48ca99edff51f424d1f140
adilkhash <adil@playrix.com>
2019-11-23 18:11:50 +0600

    добавил первый игрушечный пример из 3-х задач на Luigi

---
+++ tasks/hello_world.py
@@ -0,0 +1,40 @@
+import luigi
+
+
+class HelloTask(luigi.Task):
+    """Writes "hello" to hello.txt file"""
+    def run(self):
+        with self.output().open('w') as f:
+            f.write('hello')
+
+    def output(self):
+        return luigi.LocalTarget('hello.txt')
+
+
+class WorldTask(luigi.Task):
+    """Writes "world" to world.txt file"""
+    def run(self):
+        with self.output().open('w') as f:
+            f.write('world')
+
+    def output(self):
+        return luigi.LocalTarget('world.txt')
+
+
+class HelloWorldTask(luigi.Task):
+    """Combines two previous tasks execution and write the result to hello_world.txt"""
+    def requires(self):
+        return [
+            HelloTask(),
+            WorldTask(),
+        ]
+
+    def run(self):
+        hello, world = self.input()
+
+        with self.output().open('w') as output:
+            with hello.open() as fh, world.open() as fw:
+                output.write('{} {}'.format(fh.read(), fw.read()))
+
+    def output(self):
+        return luigi.LocalTarget('hello_world.txt')
